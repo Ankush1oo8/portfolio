710d8e7c9b473799425dc46151a0e2f3454e20e9
Ankush1oo8 <ankushchudiwal@outlook.com>
2024-09-15 02:13:53 +0530

    new config

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'src/main.jsx'), // Path to your main entry file
-        // Add other entry points if needed
-      },
-    },
-  },
 })
